ecdbb8db03383a82a9079232bce329a245141e0c
orent143 <orentgultiano11@gmail.com>
2026-05-09 23:38:00 +1200

    Added auto reply

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -115,8 +115,9 @@ const sendEmail = async () => {
     await emailjs.send(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      import.meta.env.VITE_EMAILJS_AUTO_REPLY_TEMPLATE_ID,
       templateParams,
-      import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
     )
 
         showSuccess("Message sent successfully.")
